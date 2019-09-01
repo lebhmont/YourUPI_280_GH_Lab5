085e3f432b13c7c0549b8d68e5f8b164b84d97d3
lebhmont <lebhmnont@gmail.com>
2019-09-01 14:37:45 +1200

    Commit first

---
+++ testgit/test_maths.py
@@ -0,0 +1,24 @@
+'''
+Created on Sep 1, 2019
+
+@author: Simon Meng
+'''
+import unittest     # Import the Python unit testing framework
+import maths        # Our code to test
+
+
+class MathsTest(unittest.TestCase):
+    ''' Unit tests for our maths functions. '''
+
+    def test_add(self):
+        ''' Tests the add function. '''
+        pass # TODO
+
+    def test_fibonacci(self):
+        ''' Tests the fibonacci function. '''
+        pass # TODO
+
+
+# This allows running the unit tests from the command line (python test_maths.py)
+if __name__ == '__main__':
+    unittest.main()
